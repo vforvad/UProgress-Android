190d5be767802e2677d3be28ee13ecc1ba532f28
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-21 21:16:28 +0300

    initial commit

---
+++ app/src/main/java/com/example/vsokoltsov/uprogress/AuthorizationActivity.java
@@ -0,0 +1,13 @@
+package com.example.vsokoltsov.uprogress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authorization_activity);
+    }
+}
