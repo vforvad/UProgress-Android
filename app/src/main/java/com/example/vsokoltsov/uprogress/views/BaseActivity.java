300ac6f57bf9dad0001265a64cf3b4d0088c094b
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-22 00:24:37 +0300

    config transition between base activity and Authorization activity

--- app/src/main/java/com/example/vsokoltsov/uprogress/views/BaseActivity.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/views/BaseActivity.java
@@ -1,10 +1,25 @@
 package com.example.vsokoltsov.uprogress.views;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.vsokoltsov.uprogress.R;
+import com.example.vsokoltsov.uprogress.views.authorizations.AuthorizationActivity;
 
 /**
  * Created by vsokoltsov on 22.11.16.
  */
 
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authorization_activity);
+        Intent usersActivity = new Intent(this, AuthorizationActivity.class);
+        startActivity(usersActivity);
+        overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+    }
+
 }
