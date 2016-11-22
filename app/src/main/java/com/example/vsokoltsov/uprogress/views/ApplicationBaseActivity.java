c43e0c64b69d4319638e91a5549bad39a9b296ec
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-22 17:40:55 +0300

    implement AuthorizationBaseFragment and some other stuff

---
+++ app/src/main/java/com/example/vsokoltsov/uprogress/views/ApplicationBaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.vsokoltsov.uprogress.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vsokoltsov on 22.11.16.
+ */
+
+public class ApplicationBaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/vsokoltsov/uprogress/views/authorizations/AuthorizationActivity.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/views/authorizations/AuthorizationActivity.java
@@ -2,14 +2,31 @@ package com.example.vsokoltsov.uprogress.views.authorizations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.vsokoltsov.uprogress.R;
+import com.example.vsokoltsov.uprogress.views.ApplicationBaseActivity;
 
-public class AuthorizationActivity extends AppCompatActivity {
+public class AuthorizationActivity extends ApplicationBaseActivity{
+
+    private android.support.v4.app.FragmentManager fragmentManager;
+    public AuthorizationBaseFragment authorizationBaseFragment;
+    private Toolbar mActionBarToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authorization_activity);
+        setToolbar();
+        fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        authorizationBaseFragment = new AuthorizationBaseFragment();
+        fragmentTransaction.replace(R.id.main_content, authorizationBaseFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void setToolbar() {
+        mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mActionBarToolbar);
     }
 }
