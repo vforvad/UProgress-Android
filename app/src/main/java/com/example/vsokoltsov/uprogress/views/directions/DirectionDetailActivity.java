8c5dbe324f903069636a14d49f19d01f5c985a8c
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-18 21:47:50 +0300

    change the way of redirecting back from DirectionActivity to DirectionsActivity

--- app/src/main/java/com/example/vsokoltsov/uprogress/views/directions/DirectionDetailActivity.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/views/directions/DirectionDetailActivity.java
@@ -42,20 +42,7 @@ public class DirectionDetailActivity extends ApplicationBaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent upIntent = NavUtils.getParentActivityIntent(this);
-                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-                    // This activity is NOT part of this app's task, so create a new task
-                    // when navigating up, with a synthesized back stack.
-                    TaskStackBuilder.create(this)
-                            // Add all of this activity's parents to the back stack
-                            .addNextIntentWithParentStack(upIntent)
-                            // Navigate up to the closest parent
-                            .startActivities();
-                } else {
-                    // This activity is part of this app's task, so simply
-                    // navigate up to the logical parent activity.
-                    NavUtils.navigateUpTo(this, upIntent);
-                }
+                NavUtils.navigateUpTo(this, new Intent(this, DirectionsActivity.class));
                 overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right);
                 return true;
             default:
