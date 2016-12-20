d1d350fb5cee7234f29364eb1e4f4de74e00d71d
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-21 01:02:07 +0300

    add steps icon on direction item view

--- app/src/main/java/com/example/vsokoltsov/uprogress/view_holders/DirectionViewHolder.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/view_holders/DirectionViewHolder.java
@@ -1,8 +1,13 @@
 package com.example.vsokoltsov.uprogress.view_holders;
 
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.vsokoltsov.uprogress.R;
@@ -21,6 +26,11 @@ public class DirectionViewHolder extends RecyclerView.ViewHolder implements View
     public TextView directionPercents;
     private Direction direction;
     private DirectionItemClickListener callbacks;
+    private ImageView stepsIcon;
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     public DirectionViewHolder(View itemView) {
         super(itemView);
@@ -33,7 +43,10 @@ public class DirectionViewHolder extends RecyclerView.ViewHolder implements View
         cv = (CardView) itemView.findViewById(R.id.directionItem);
         directionTitle= (TextView) itemView.findViewById(R.id.directionTitle);
         directionPercents = (TextView)itemView.findViewById(R.id.directionPercents);
-
+        Drawable stepsIconImg = AppCompatResources.getDrawable(adapter.fragment.getActivity().getApplicationContext(), R.drawable.steps);
+//        Drawable stepsIconImg = adapter.fragment.getActivity().getApplicationContext().getResources().getDrawable( R.drawable.steps);
+        stepsIcon = (ImageView) itemView.findViewById(R.id.stepsIcon);
+        stepsIcon.setImageDrawable(stepsIconImg);
         cv.setOnClickListener(this);
     }
 
