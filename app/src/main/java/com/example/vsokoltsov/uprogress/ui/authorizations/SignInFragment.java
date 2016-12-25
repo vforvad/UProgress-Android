5c4da731accd191eba233f17c988763e655a522d
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-25 22:24:37 +0300

    remove useless code from signInFragment

--- app/src/main/java/com/example/vsokoltsov/uprogress/ui/authorizations/SignInFragment.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/ui/authorizations/SignInFragment.java
@@ -38,7 +38,7 @@ import rx.schedulers.Schedulers;
  * Created by vsokoltsov on 22.11.16.
  */
 
-public class SignInFragment extends Fragment implements Button.OnClickListener {
+public class SignInFragment extends Fragment{
     private View fragmentView;
     private ApplicationBaseActivity activity;
     private EditText emailField;
@@ -59,61 +59,14 @@ public class SignInFragment extends Fragment implements Button.OnClickListener {
                 (EditText) fragmentView.findViewById(R.id.passwordField),
                 button
         );
-
+        // Set fields in viewHolder
         viewHolder.setFields(getContext());
 
         final SignInModel model = new SignInModelImpl(viewHolder);
         final SignInView view = new SignInViewImpl(viewHolder);
         presenter = new SignInPresenterImpl(model, view);
         presenter.onCreate(this);
-//        setIconsForFields();
-//        Button signInButton = (Button) fragmentView.findViewById(R.id.signInButton);
-//        signInButton.setOnClickListener(this);
+
         return fragmentView;
     }
-
-    private void setIconsForFields() {
-        emailField = (EditText) fragmentView.findViewById(R.id.emailField);
-        passwordField = (EditText) fragmentView.findViewById(R.id.passwordField);
-
-        Drawable emailImg = getContext().getResources().getDrawable( R.drawable.email);
-        Drawable passwordImg= getContext().getResources().getDrawable( R.drawable.password);
-
-        emailField.setCompoundDrawablesWithIntrinsicBounds( emailImg, null, null, null);
-        passwordField.setCompoundDrawablesWithIntrinsicBounds( passwordImg, null, null, null);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        presenter.onSignInSubmit();
-    }
-
-//    @Override
-//    public void onClick(View v) {
-//        SignInRequest user = new SignInRequest(emailField.getText().toString(),
-//                passwordField.getText().toString());
-//        Retrofit retrofit = ApiRequester.getInstance().getRestAdapter();
-//        UserApi service = retrofit.create(UserApi.class);
-//        service.signIn(user)
-
-////        activity.showProgress(R.string.sign_in_action);
-//
-//    }
-//
-//    private void  successAuth(Token token) {
-//        String str = token.getToken();
-//        ApiRequester.getInstance().setToken(token.getToken());
-//        activity.currentUserRequest();
-//    }
-//
-//    private void handleErrors(Throwable t) throws IOException {
-//        RetrofitException error = (RetrofitException) t;
-//        t.printStackTrace();
-//        ErrorResponse errors = error.getErrorBodyAs(ErrorResponse.class);
-//        emailField.setError(errors.getFullErrorMessage("email"));
-//        passwordField.setError(errors.getFullErrorMessage("password"));
-//
-////        activity.dismissProgress();
-//    }
 }
