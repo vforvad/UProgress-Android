0f8920e27498e750dc29091892449c4ce04f6464
Vadim Sokoltsov <vforvad@gmail.com>
2017-01-01 01:40:29 +0300

    refactored a little bit the DirectionsListFragment

--- app/src/main/java/com/example/vsokoltsov/uprogress/directions_list/ui/DirectionsListFragment.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/directions_list/ui/DirectionsListFragment.java
@@ -43,21 +43,27 @@ public class DirectionsListFragment extends Fragment implements SwipeRefreshLayo
     private List<Direction> directions = new ArrayList<Direction>();
     private RecyclerView rv;
     private DirectionsListAdapter adapter;
-    private AuthorizationService authManager = AuthorizationService.getInstance();
     private SwipeRefreshLayout swipeLayout;
     private LinearLayoutManager llm;
     private boolean canLoad = true;
     private DirectionsListPresenter presenter;
+    private User user;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        User user = AuthorizationService.getInstance().getCurrentUser();
+        user = AuthorizationService.getInstance().getCurrentUser();
         activity = (ApplicationBaseActivity) getActivity();
-
         fragmentView = inflater.inflate(R.layout.directions_list_fragment, container, false);
+        setElements();
+        setComponents();
+        presenter.loadDirections();
+        return fragmentView;
+    }
+
+    private void setElements() {
         rv = (RecyclerView) fragmentView.findViewById(R.id.directionsList);
         adapter = new DirectionsListAdapter(directions, this);
         llm = new LinearLayoutManager(getActivity());
@@ -65,115 +71,19 @@ public class DirectionsListFragment extends Fragment implements SwipeRefreshLayo
         rv.setLayoutManager(llm);
         swipeLayout = (SwipeRefreshLayout) fragmentView.findViewById(R.id.swipe_layout);
         swipeLayout.setOnRefreshListener(this);
+    }
 
-
-
+    private void setComponents() {
         final DirectionModel model = new DirectionModelImpl();
         presenter = new DirectionsListPresenterImpl(this, model, user);
         setOnClickListener();
         setOnScrollListener();
-        presenter.loadDirections();
-
-
-//        progressBar = (ProgressBar) fragmentView.findViewById(R.id.progressBar);
-//        rv.setHasFixedSize(true);
-
-//        rv.setLayoutManager(llm);
-//        setAdapter();
-//        rv.setAdapter(adapter);
-//        rv.setOnScrollListener(new RecyclerView.OnScrollListener(){
-//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-//                super.onScrolled(recyclerView, dx, dy);
-//                if (canLoad) {
-
-//                }
-//
-//            }
-//        });
-//        loadDirectionsList();
-        return fragmentView;
     }
 
     @Override
     public void onRefresh() {
         presenter.refreshList();
     }
-
-//    private void setAdapter() {
-//        adapter = new DirectionsListAdapter(directions, this);
-//    }
-
-//    private void loadDirectionsList() {
-//        canLoad = false;
-////        if (showMainLoader) {
-////            activity.showProgress(R.string.loading);
-////        }
-//        progressBar.setVisibility(View.VISIBLE);
-//        User currentUser = authManager.getCurrentUser();
-//        Retrofit retrofit = api.getRestAdapter();
-//        DirectionsApi service = retrofit.create(DirectionsApi.class);
-//        service.getDirections(currentUser.getNick(), pageNumber)
-//                .subscribeOn(Schedulers.newThread())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<DirectionsList>() {
-//                    @Override
-//                    public void onCompleted() {
-//                        swipeLayout.setRefreshing(false);
-//                        progressBar.setVisibility(View.GONE);
-//                        showMainLoader = true;
-//                        canLoad = true;
-//                        int firstVisibleItem = llm.findFirstVisibleItemPosition();
-//                        firstVisibleInListview = firstVisibleItem;
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onNext(DirectionsList directionsList) {
-//                        setDirectionsList(directionsList);
-//                    }
-//                });
-//    }
-//
-//    private void setDirectionsList(DirectionsList directions) {
-//        if (!showMainLoader) {
-//            adapter.directions = directions.getDirections();
-//        }
-//        else {
-//            for(int i = 0; i < directions.getDirections().size(); i++) {
-//                Direction d = (Direction) directions.getDirections().get(i);
-//                adapter.directions.add(d);
-//            }
-//        }
-//        adapter.notifyDataSetChanged();
-//    }
-
-//    @Override
-//    public void onItemClicked(Direction direction) {
-//        Intent detailIntent = new Intent(getActivity(), DirectionDetailActivity.class);
-//        detailIntent.putExtra("direction", direction);
-//        PendingIntent pendingIntent =
-//                TaskStackBuilder.create(getActivity())
-//                        // add all of DetailsActivity's parents to the stack,
-//                        // followed by DetailsActivity itself
-//                        .addNextIntentWithParentStack(detailIntent)
-//                        .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity());
-//        builder.setContentIntent(pendingIntent);
-//        startActivity(detailIntent);
-//        getActivity().overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
-//    }
-//
-//    @Override
-//    public void onRefresh() {
-//        pageNumber = 1;
-//        showMainLoader = false;
-//        loadDirectionsList();
-//    }
 
     private void setOnClickListener() {
         adapter.getPositionClicks()
