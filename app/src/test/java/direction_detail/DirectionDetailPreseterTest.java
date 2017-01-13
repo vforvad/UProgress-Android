c9da9a63a4c888a203c3bd34bbc17a2576363c58
Vadim Sokoltsov <vforvad@gmail.com>
2017-01-13 15:30:01 +0300

    add test for failed loading of direction

--- app/src/test/java/direction_detail/DirectionDetailPreseterTest.java
+++ app/src/test/java/direction_detail/DirectionDetailPreseterTest.java
@@ -4,6 +4,9 @@ import com.example.vsokoltsov.uprogress.authentication.presenters.Authentication
 import com.example.vsokoltsov.uprogress.direction_detail.model.DirectionDetail;
 import com.example.vsokoltsov.uprogress.direction_detail.model.DirectionDetailModel;
 import com.example.vsokoltsov.uprogress.direction_detail.model.DirectionDetailModelImpl;
+import com.example.vsokoltsov.uprogress.direction_detail.model.steps.Step;
+import com.example.vsokoltsov.uprogress.direction_detail.model.steps.StepRequest;
+import com.example.vsokoltsov.uprogress.direction_detail.model.steps.StepResponse;
 import com.example.vsokoltsov.uprogress.direction_detail.presenter.DirectionDetailPresenter;
 import com.example.vsokoltsov.uprogress.direction_detail.presenter.DirectionDetailPresenterImpl;
 import com.example.vsokoltsov.uprogress.direction_detail.view.DirectionDetailView;
@@ -32,6 +35,7 @@ import static org.mockito.Mockito.when;
 public class DirectionDetailPreseterTest {
     String userId = "1";
     String directionId = "1";
+    String stepId = "1";
 
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
@@ -40,6 +44,7 @@ public class DirectionDetailPreseterTest {
     public RxJavaSchedulersTestRule rxJavaSchedulersTestRule = new RxJavaSchedulersTestRule();
 
     DirectionDetailPresenterImpl presenter;
+    Throwable t;
 
     @Mock
     DirectionDetailModel model;
@@ -57,6 +62,7 @@ public class DirectionDetailPreseterTest {
     @Before
     public void setUp() throws Exception {
         presenter = new DirectionDetailPresenterImpl(model, view);
+        t = new Throwable();
     }
 
     @Test
@@ -71,36 +77,101 @@ public class DirectionDetailPreseterTest {
         verify(view, times(1)).stopLoader();
     }
 
-//    @Test
-//    public void failedLoadDirectionTest() {
+    @Test
+    public void failedLoadDirectionTest() {
+        when(detail.getDirection()).thenReturn(direction);
+        when(model.loadDirection(userId, directionId)).thenReturn(Observable.error(t));
+
+        presenter.loadDirection(userId, directionId);
+
+        verify(view, times(1)).startLoader();
+        verify(view, times(1)).failureResponse(t);
+        verify(view, times(1)).stopLoader();
+    }
+
+//    @Mock
+//    Step step;
 //
-//    }
+//    @Mock
+//    StepRequest stepRequest;
+//
+//    @Mock
+//    StepResponse stepResponse;
 //
 //    @Test
 //    public void successUpdateStepTest() {
+//        when(stepResponse.getStep()).thenReturn(step);
+//        when(model.updateStep(userId, directionId, stepId, stepRequest)).thenReturn(Observable.just(stepResponse));
 //
+//        presenter.updateStep(userId, directionId, stepId, stepRequest);
+//
+//        verify(view, times(1)).startLoader();
+//        verify(view, times(1)).successStepUpdate(step);
+//        verify(view, times(1)).stopLoader();
 //    }
 //
 //    @Test
 //    public void failedUpdateStepTest() {
+//        when(stepResponse.getStep()).thenReturn(step);
+//        when(model.updateStep(userId, directionId, stepId, stepRequest)).thenReturn(Observable.error(t));
 //
+//        presenter.updateStep(userId, directionId, stepId, stepRequest);
+//
+//        verify(view, times(1)).startLoader();
+//        verify(view, times(1)).failureStepUpdate(t);
+//        verify(view, times(1)).stopLoader();
 //    }
 //
 //    @Test
-//    public void reloadDirectionTest() {
+//    public void successReloadDirectionTest() {
+//        when(detail.getDirection()).thenReturn(direction);
+//        when(model.loadDirection(userId, directionId)).thenReturn(Observable.just(detail));
 //
+//        presenter.reloadDirection(userId, directionId);
+//
+//        verify(view, times(1)).onStartRefresh();
+//        verify(view, times(1)).successResponse(direction);
+//        verify(view, times(1)).onStopRefresh();
+//    }
+//
+//    @Test
+//    public void failedReloadDirectionTest() {
+//        when(detail.getDirection()).thenReturn(direction);
+//        when(model.loadDirection(userId, directionId)).thenReturn(Observable.error(t));
+//
+//        presenter.reloadDirection(userId, directionId);
+//
+//        verify(view, times(1)).onStartRefresh();
+//        verify(view, times(1)).failureResponse(t);
+//        verify(view, times(1)).onStopRefresh();
 //    }
 //
 //    @Test
 //    public void successDeleteStepTest() {
+//        int[] positions =  new int[1];
+//        when(stepResponse.getStep()).thenReturn(step);
+//        when(model.deleteStep(userId, directionId, stepId)).thenReturn(Observable.just(stepResponse));
 //
+//        presenter.deleteStep(userId, directionId, stepId, positions);
+//
+//        verify(view, times(1)).startLoader();
+//        verify(view, times(1)).successDelete(step, positions);
+//        verify(view, times(1)).stopLoader();
 //    }
 //
 //    @Test
 //    public void failedDeleteStepTest() {
+//        int[] positions =  new int[1];
+//        when(stepResponse.getStep()).thenReturn(step);
+//        when(model.deleteStep(userId, directionId, stepId)).thenReturn(Observable.error(t));
 //
+//        presenter.deleteStep(userId, directionId, stepId, positions);
+//
+//        verify(view, times(1)).startLoader();
+//        verify(view, times(1)).failedDelete(t);
+//        verify(view, times(1)).stopLoader();
 //    }
-//
+
 //    @Test
 //    public void successCreateStepTest() {
 //
