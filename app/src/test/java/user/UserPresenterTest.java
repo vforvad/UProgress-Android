7934ca9633b84ac2472d08db31202327138c9294
Vadim Sokoltsov <vforvad@gmail.com>
2017-01-13 18:35:27 +0300

    add specs for user presenter

---
+++ app/src/test/java/user/UserPresenterTest.java
@@ -0,0 +1,67 @@
+package user;
+
+import com.example.vsokoltsov.uprogress.direction_detail.presenter.DirectionDetailPresenterImpl;
+import com.example.vsokoltsov.uprogress.user.current.User;
+import com.example.vsokoltsov.uprogress.user.current.UserRequest;
+import com.example.vsokoltsov.uprogress.user.current.UserResponse;
+import com.example.vsokoltsov.uprogress.user.models.UserModel;
+import com.example.vsokoltsov.uprogress.user.presenters.UserProfilePresenterImpl;
+import com.example.vsokoltsov.uprogress.user.views.UserProfileView;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import rx.Observable;
+import rx.plugins.RxJavaSchedulersTestRule;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by vsokoltsov on 13.01.17.
+ */
+
+public class UserPresenterTest {
+    String userId = "1";
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Rule
+    public RxJavaSchedulersTestRule rxJavaSchedulersTestRule = new RxJavaSchedulersTestRule();
+
+    UserProfilePresenterImpl presenter;
+
+    @Mock Throwable t;
+    @Mock UserModel model;
+    @Mock UserProfileView view;
+    @Mock UserResponse userResponse;
+    @Mock UserRequest userRequest;
+    @Mock User user;
+
+    @Before
+    public void setUp() throws Exception {
+        presenter = new UserProfilePresenterImpl(view, model);
+    }
+
+    @Test
+    public void successUserProfileUpdate() {
+        when(userResponse.getUser()).thenReturn(user);
+        when(model.updateUser(userId, userRequest)).thenReturn(Observable.just(userResponse));
+
+        presenter.updateUser(userId, userRequest);
+
+        verify(view, times(1)).startLoader();
+        verify(view, times(1)).successUpdate(user);
+        verify(view, times(1)).stopLoader();
+    }
+
+    @Test
+    public void failedUserProfileUpdate() {
+
+    }
+}
