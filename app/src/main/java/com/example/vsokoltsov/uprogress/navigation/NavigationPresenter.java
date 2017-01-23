afa447086e7dc4f81053a422af7fbfea1eced1f8
Vadim Sokoltsov <vforvad@gmail.com>
2017-01-23 20:48:32 +0300

    add the rest of activities

--- app/src/main/java/com/example/vsokoltsov/uprogress/navigation/NavigationPresenter.java
+++ app/src/main/java/com/example/vsokoltsov/uprogress/navigation/NavigationPresenter.java
@@ -14,9 +14,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.vsokoltsov.uprogress.R;
+import com.example.vsokoltsov.uprogress.authentication.ui.AuthorizationActivity;
 import com.example.vsokoltsov.uprogress.common.ApplicationBaseActivity;
 import com.example.vsokoltsov.uprogress.common.BaseApplication;
 import com.example.vsokoltsov.uprogress.directions_list.ui.DirectionsActivity;
+import com.example.vsokoltsov.uprogress.statistics.ui.StatisticsActivity;
 import com.example.vsokoltsov.uprogress.user.current.User;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -138,9 +140,21 @@ public class NavigationPresenter implements NavigationView.OnNavigationItemSelec
         drawerLayout.closeDrawers();
 
         switch(item.getItemId()) {
+            case R.id.sign_in:
+                Intent signInActivity = new Intent(context, AuthorizationActivity.class);
+                signInActivity.putExtra("action", "sign_in");
+                context.startActivity(signInActivity);
+            case R.id.sign_up:
+                Intent signUpActivity = new Intent(context, AuthorizationActivity.class);
+                signUpActivity.putExtra("action", "sign_up");
+                context.startActivity(signUpActivity);
             case R.id.directions:
                 Intent dirActivity = new Intent(context, DirectionsActivity.class);
                 context.startActivity(dirActivity);
+                return true;
+            case R.id.statistics:
+                Intent statisticsActivity = new Intent(context, StatisticsActivity.class);
+                context.startActivity(statisticsActivity);
                 return true;
             default: return true;
         }
