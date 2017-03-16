9150c718b3de0119e6778b2d4b2862bddcb5b543
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-17 00:34:48 +0300

    Add boilerplate for tablet support

---
+++ app/src/main/java/com/vsokoltsov/uprogress/common/TabletActivity.java
@@ -0,0 +1,17 @@
+package com.vsokoltsov.uprogress.common;
+
+import android.os.Bundle;
+
+import com.vsokoltsov.uprogress.R;
+import com.vsokoltsov.uprogress.common.utils.ContextManager;
+
+/**
+ * Created by vsokoltsov on 17.03.17.
+ */
+
+public class TabletActivity extends ApplicationBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
